e8b4aefa578f19b667848d15a7668646a063d0dc
akshay <akshayd00@outlook.com>
2020-12-01 17:55:17 +0530

    old_school_keypad problem solved

--- old_school_keypad/helper.py
+++ old_school_keypad/helper.py
@@ -15,20 +15,71 @@ contact_list = {
     'dad': 9090909090,
     'sis': 9988776655,
     'friend': 9876543210,
+    'faf': 9191919191,
 }
 
 
-def smart_contact_finder(usr_inp=''):
+contact_name_to_keypad_combinations = {}
+
+
+def smart_contact_finder(user_input=''):
     """Helper function to fetch possible contact."""
+    if not user_input:
+        return []
+
+    matched_contact_numbers = []
+
+    matched_keypad_numbers = list(
+        filter(
+            lambda keypad_number: user_input in keypad_number,
+            contact_name_to_keypad_combinations.keys()
+        )
+    )
+
+    for keypad_number in matched_keypad_numbers:
+        matched_contact_numbers.extend(
+            contact_name_to_keypad_combinations[keypad_number]
+        )
+
+    return matched_contact_numbers
+
+
+def contact_name_to_keypad_number_converter(contact_list={}):
+    """Converts Alphabatical Contact names to Keypad digits and store in-place.
+    """
+    for key, value in contact_list.items():
+        raw_contact_name = key
+        raw_contact_number = value
+
+        keypad_numbered_contact_name = ''
+
+        for character in key:
+            for digit, letters in alpha_numeric_mapper.items():
+                if character in letters:
+                    keypad_numbered_contact_name += str(digit)
+
+        if not keypad_numbered_contact_name:
+            return
+
+        try:
+            assert type(contact_name_to_keypad_combinations[keypad_numbered_contact_name]) is list
+        except (AssertionError, KeyError):
+            contact_name_to_keypad_combinations[keypad_numbered_contact_name] = [raw_contact_number, ]
+        else:
+            contact_name_to_keypad_combinations[keypad_numbered_contact_name].append(raw_contact_number)
+
     return
 
 
 if __name__ == '__main__':
     dummy_test_cases = {
-        '323': contact_list['dad'],
+        '323': [contact_list['dad'], contact_list['faf'], ],
         '3743': contact_list['friend'],
-        '99999': None
+        '99999': [],
     }
+
+    # Create a rainbow table of contact_name-keypad_number combinations
+    contact_name_to_keypad_number_converter(contact_list)
 
     score = 0
 
@@ -36,7 +87,7 @@ if __name__ == '__main__':
         try:
             predicted_response = smart_contact_finder(key)
 
-            assert predicted_response == value
+            assert value in predicted_response or value == predicted_response
         except AssertionError:
             print(f'Test case failed | Excepected result: {value} | Actual result: {predicted_response}')
         else:
